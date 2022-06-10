95fcee53b50f76cd282f3ad63935581ea8bd0ef4
Mateusz Bonar <mateusz211@vp.pl>
2022-06-10 23:36:17 +0200

    test-protected-branch

--- my-app/src/components/Nav/NavItem.tsx
+++ my-app/src/components/Nav/NavItem.tsx
@@ -1,6 +1,6 @@
 import React, {FC} from "react";
 
-export type NavItemType = { name: string, href: string, handleClick?: () =>  void}
+export type NavItemType =  { name: string, href: string, handleClick?: () =>  void}
 
 const NavItem: FC<NavItemType> = ({name, href,handleClick}): JSX.Element =>
     <li>
