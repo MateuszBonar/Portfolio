7409f6e8b3c0539175698faedd792ae73dc19004
Mateusz Bonar <mateusz211@vp.pl>
2022-06-10 23:27:46 +0200

    add-new-type

--- my-app/src/components/Nav/NavItem.tsx
+++ my-app/src/components/Nav/NavItem.tsx
@@ -1,7 +1,8 @@
 import React, {FC} from "react";
 
+export type NavItemType = { name: string, href: string, handleClick?: () =>  void}
 
-const NavItem: FC<{ name: string, href: string, handleClick?: () =>  void}> = ({name, href,handleClick}): JSX.Element =>
+const NavItem: FC<NavItemType> = ({name, href,handleClick}): JSX.Element =>
     <li>
         <a href={href} onClick={handleClick && handleClick}>{name}</a>
     </li>
